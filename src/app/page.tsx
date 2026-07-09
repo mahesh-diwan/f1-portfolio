4cc0361d50575557911022ae1407902bb0446d82
Mahesh Diwan <diwanmahesh11@gmail.com>
2026-07-09 18:04:30 +0530

    fix: sticky footer via flex column layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import { Footer } from "@/components/layout/Footer";
 import { ScrollProgress } from "@/components/layout/ScrollProgress";
 import { CommandPalette } from "@/components/layout/CommandPalette";
 import { SectionRouter } from "@/components/layout/SectionRouter";
-import { KonamiCodeOverlay } from "@/components/ui/KonamiCode";
+import { KonamiCodeOverlay } from "@/components/providers/KonamiCode";
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navigation />
       <CommandPalette />
       <ScrollProgress />
       <KonamiCodeOverlay />
-      <main id="main-content">
+      <main id="main-content" className="flex-1">
         <SectionRouter />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
