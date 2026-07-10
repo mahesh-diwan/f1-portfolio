9fa82064c44f4357b4ff954b2725a1b880441254
Mahesh Diwan <diwanmahesh11@gmail.com>
2026-07-10 15:51:55 +0530

    chore: delete unused exports from portfolio.ts and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,23 +2,4 @@ export function cn(...classes: (string | boolean | undefined | null)[]): string 
   return classes.filter(Boolean).join(" ");
 }
 
-export function clamp(value: number, min: number, max: number): number {
-  return Math.min(Math.max(value, min), max);
-}
 
-export function mapRange(
-  value: number,
-  inMin: number,
-  inMax: number,
-  outMin: number,
-  outMax: number,
-): number {
-  return ((value - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin;
-}
-
-export function formatNumber(num: number): string {
-  if (num >= 1000) {
-    return (num / 1000).toFixed(1) + "k";
-  }
-  return num.toString();
-}
